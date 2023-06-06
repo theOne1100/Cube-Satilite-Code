4fb7721d0a3d026f91f83d3a6552645890980b00
Surya <123630128+theOne1100@users.noreply.github.com>
2023-06-06 20:06:27 +1200

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+"""
+
+Initalisation - micro:bit doesn't like me naming variables called "Light", so the var name is "Light2"
+
+"""
 Light2 = 0
 radio.set_group(216)
 pins.digital_write_pin(DigitalPin.P1, 0)
 pins.digital_write_pin(DigitalPin.P0, 0)
 radio.send_string("Starting Brodcast...")
+"""
+
+This code starts in radio group 216, where it will display
+
+"Starting Brodcast...". After that, every 5 secs, it will brodcast its
+
+temperature and compass heading. Also, it will send alternating signals
+
+to pin 1 and pin 2, so that 2 LEDS can be sttached and the lights
+
+will alternate between on and off, creating the illusion of processing.
+
+"""
+# Light change function
 
 def on_every_interval():
     global Light2
@@ -17,6 +36,8 @@ def on_every_interval():
     else:
         pass
 loops.every_interval(500, on_every_interval)
+
+# Compass and temperature function
 
 def on_every_interval2():
     radio.send_string("Temp: " + ("" + str(input.temperature())))
--- main.ts
+++ main.ts
@@ -1,10 +1,24 @@
+/**
+ * This code starts in radio group 216, where it will display
+ * 
+ * "Starting Brodcast...". After that, every 5 secs, it will brodcast its
+ * 
+ * temperature and compass heading. Also, it will send alternating signals
+ * 
+ * to pin 1 and pin 2, so that 2 LEDS can be sttached and the lights
+ * 
+ * will alternate between on and off, creating the illusion of processing.
+ */
+/**
+ * Initalisation - micro:bit doesn't like me naming variables called "Light", so the var name is "Light2"
+ */
 let Light2 = 0
 radio.setGroup(216)
 pins.digitalWritePin(DigitalPin.P1, 0)
 pins.digitalWritePin(DigitalPin.P0, 0)
 radio.sendString("Starting Brodcast...")
-loops.everyInterval(500, function on_every_interval() {
-    
+// Light change function
+loops.everyInterval(500, function () {
     if (Light2 == 0) {
         pins.digitalWritePin(DigitalPin.P1, 0)
         pins.digitalWritePin(DigitalPin.P0, 1)
@@ -14,11 +28,11 @@ loops.everyInterval(500, function on_every_interval() {
         pins.digitalWritePin(DigitalPin.P0, 0)
         Light2 = 0
     } else {
-        
+    	
     }
-    
 })
-loops.everyInterval(5000, function on_every_interval2() {
-    radio.sendString("Temp: " + ("" + ("" + input.temperature())))
-    radio.sendString("Compass: " + ("" + ("" + input.compassHeading())))
+// Compass and temperature function
+loops.everyInterval(5000, function () {
+    radio.sendString("Temp: " + ("" + input.temperature()))
+    radio.sendString("Compass: " + ("" + input.compassHeading()))
 })
